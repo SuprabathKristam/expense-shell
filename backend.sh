b93ee8c99e1552ede6b9911f1f415d450fde255b
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-01 12:20:50 +0530

    Used log file as a variable

--- backend.sh
+++ backend.sh
@@ -1,43 +1,43 @@
 log_file = $log_file
 
 echo -e "\e[36mDisable default version of NodJs\e[0m"
-dnf module disable nodejs -y &>> $log_file
+dnf module disable nodejs -y &>>$log_file
 
 echo -e "\e[36mEnable NodeJs 18 version\e[0m"
-dnf module enable nodejs:18 -y &>> $log_file
+dnf module enable nodejs:18 -y &>>$log_file
 
 echo -e "\e[36mInstalling NodeJs\e[0m"
-dnf install nodejs -y &>> $log_file
+dnf install nodejs -y &>>$log_file
 
 echo -e "\e[36mConfiguring backend service file\e[0m"
-cp backend.service /etc/systemd/system/backend.service &>> $log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
 echo -e "\e[36mAdding Application user\e[0m"
-useradd expense &>> $log_file
+useradd expense &>>$log_file
 
 echo -e "\e[36mRemoving existing default app content\e[0m"
-rm -rf /app &>> $log_file
+rm -rf /app &>>$log_file
 
 echo -e "\e[36mCreating Application Directory\e[0m"
-mkdir /app &>> $log_file
+mkdir /app &>>$log_file
 
 echo -e "\e[36mDownloading application content\e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>> $log_file
-cd /app &>> $log_file
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
+cd /app &>>$log_file
 
 echo -e "\e[36mExtracting Application Content\e[0m"
-unzip /tmp/backend.zip &>> $log_file
+unzip /tmp/backend.zip &>>$log_file
 
 echo -e "\e[36mDownloading Application dependencies\e[0m"
-npm install &>> $log_file
+npm install &>>$log_file
 
 echo -e "\e[36mReloading systemd and start backend service\e[0m"
-systemctl daemon-reload &>> $log_file
-systemctl enable backend &>> $log_file
-systemctl restart backend &>> $log_file
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl restart backend &>>$log_file
 
 echo -e "\e[36mInstalling  MySql Clinet\e[0m"
-dnf install mysql -y &>> $log_file
+dnf install mysql -y &>>$log_file
 
 echo -e "\e[36mLoading Schema\e[0m"
-mysql -h mysql-dev.kdevopspractice.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>> $log_file
+mysql -h mysql-dev.kdevopspractice.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
