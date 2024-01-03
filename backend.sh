82292f61a2a1f7a2808b84238953e969b7132ae9
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-03 22:19:34 +0530

    Learning return and exit

--- backend.sh
+++ backend.sh
@@ -6,42 +6,55 @@ Head(){
 }
 Head "Disable default version of NodJs"
 dnf module disable nodejs -y &>>$log_file
+echo $?
 
 Head "Enable NodeJs 18 version"
 dnf module enable nodejs:18 -y &>>$log_file
+echo $?
 
 Head "nstalling NodeJs"
 dnf install nodejs -y &>>$log_file
+echo $?
 
 Head "Configuring backend service"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
+echo $?
 
 Head "Adding Application user"
 useradd expense &>>$log_file
+echo $?
 
 Head "Removing existing default app content"
 rm -rf /app &>>$log_file
+echo $?
 
 Head "Creating Application Directory"
 mkdir /app &>>$log_file
+echo $?
 
 Head "Downloading application content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
-cd /app &>>$log_file
+echo $?
+cd /app
 
 Head "Extracting Application Content"
 unzip /tmp/backend.zip &>>$log_file
+echo $?
 
 Head "Downloading Application dependencies"
 npm install &>>$log_file
+echo $?
 
 Head "Reloading systemd and start backend service"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
+echo $?
 
 Head "Installing  MySql Clinet"
 dnf install mysql -y &>>$log_file
+echo $?
 
 Head "Loading Schema"
 mysql -h mysql-dev.kdevopspractice.online -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>$log_file
+echo $?
