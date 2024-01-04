61d26020c5e157b67f8c67c10dab075593a0e691
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-04 07:20:40 +0530

    enhancing the code in front end

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,32 @@
-dnf install nginx -y
-cp expense.conf /etc/nginx/default.d/expense.conf
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+log_file=/tmp/expense.log
+
+Head(){
+  echo -e "\e[36m$1\e[0m"
+}
+
+Head "Installing Nginx"
+dnf install nginx -y &>>$log_file
+echo $?
+
+Head "Copying expense config"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo $?
+
+Head "Deleting ol/default content"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+echo $?
+
+Head "Downloading app content"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo $?
+
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl restart nginx
+
+Head "Extracting the application content"
+unzip /tmp/frontend.zip &>>$log_file
+echo $?
+
+Head "Enabling and restarting nginx"
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
+echo $?
