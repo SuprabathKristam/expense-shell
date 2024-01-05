e389d413c28db8a1bd589008ad76a2e3267f3955
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-05 22:08:16 +0530

    Implementing if condition

--- backend.sh
+++ backend.sh
@@ -5,7 +5,6 @@ component=backend
 
 Head "Disable default version of NodJs"
 dnf module disable nodejs -y &>>$log_file
-
 if [ "$?" -eq 0 ];then
   echo SUCCESS
 else
@@ -15,37 +14,77 @@ fi
 
 Head "Enable NodeJs 18 version"
 dnf module enable nodejs:18 -y &>>$log_file
-echo $?
+if [ "$?" -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 Head "Installing NodeJs"
 dnf install nodejs -y &>>$log_file
-echo $?
+if [ "$?" -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 Head "Configuring backend service"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-echo $?
+if [ "$?" -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 Head "Adding Application user"
 useradd expense &>>$log_file
-echo $?
+if [ "$?" -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 App_Prereq "/app" # Added this function in common.sh with steps that are repeating in other
 #Files also and using it here
 
 Head "Downloading Application dependencies"
 npm install &>>$log_file
-echo $?
+if [ "$?" -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 Head "Reloading systemd and start backend service"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
-echo $?
+if [ "$?" -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 Head "Installing  MySql Clinet"
 dnf install mysql -y &>>$log_file
-echo $?
+if [ "$?" -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 Head "Loading Schema"
 mysql -h mysql-dev.kdevopspractice.online -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>$log_file
-echo $?
+if [ "$?" -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
