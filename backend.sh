63e1d549a7746412e24079c62305e70b38fd678b
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-08 20:35:00 +0530

    Implementing exit code and messages

--- backend.sh
+++ backend.sh
@@ -20,7 +20,10 @@ cp backend.service /etc/systemd/system/backend.service &>>$log_file
 Stat $?
 
 Head "Adding Application user"
-useradd expense &>>$log_file
+id expense $>>log_file
+if [ $? ne 0 ]; then
+  useradd expense &>>$log_file
+fi
 Stat $?
 
 App_Prereq "/app" # Added this function in common.sh with steps that are repeating in other
