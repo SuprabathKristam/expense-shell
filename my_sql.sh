840ae27d4e7d95d0e299538a118fc279259585dc
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-09 20:33:02 +0530

    Automating msql setup

--- my_sql.sh
+++ my_sql.sh
@@ -7,22 +7,22 @@ source common.sh # Importing functions and variables from common.sh file
 component=my_sql
 
 Head "Disable default version of mysql"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>$log_file
 Stat $?
 
 Head "Setting mysql repo file"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 Stat $?
 
 Head "Installing Mysql Server"
-dnf install mysql-community-server -y
+dnf install mysql-community-server -y &>>$log_file
 Stat $?
 
 Head "Restarting mysqld service"
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
 Stat $?
 
 Head "Setting default password"
-mysql_secure_installation --set-root-pass ${MYSQL_PASSWORD=$1}
+mysql_secure_installation --set-root-pass ${MYSQL_PASSWORD=$1} &>>$log_file
 Stat $?
